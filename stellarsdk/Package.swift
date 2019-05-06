118eeebca87d303373b1768f56390e061cde6fc8
Razvan Chelemen <chelemen.razvan@gmail.com>
2019-05-06 17:08:26 +0300

    Add swift package manager manifest

---
+++ stellarsdk/Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "stellar-ios-mac-sdk",
+    products: [
+        .library(name: "stellar-ios-mac-sdk", targets: ["stellarsdk"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "stellarsdk",
+            dependencies: [],
+            path: "stellarsdk"),
+        .testTarget(
+            name: "stellarsdkTests",
+            dependencies: ["stellarsdk"],
+            path: "stellarsdkTests"),
+    ]
+)
